3dd9787fdd04e945b511ada79190f7830b91a447
Marlon Agno <mdj.agno@gmail.com>
2019-09-11 15:24:45 -0400

    Rendered MainPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import DUMMYSTORE from './dummystore'
 import Header from './Header/Header';
 import NavBar from './NavBar/NavBar';
 import MainPage from './MainPage/MainPage';
 import NotePage from './NotePage/NotePage';
 import FolderPage from './FolderPage/FolderPage';
 
-function App() {
+class App extends React.Component {
+state = {
+  store: DUMMYSTORE
+}
+
+
+  render(){
+    console.log(this.state.store)
+
   return (
     <BrowserRouter>
       <div className="App">
@@ -15,7 +24,8 @@ function App() {
       <Header />
       <NavBar />
       <Switch>
-        <Route exact path="/" component={MainPage} />
+        <Route exact path="/" render={(props) => <MainPage {...props} store={this.state.store} />}
+         />
         <Route path="/folderPage" component={FolderPage} />
         <Route path="/notePage" component={NotePage} />   
         <Route render={() => <h2>Page Not Found</h2>}/>
@@ -24,8 +34,9 @@ function App() {
                     
       </div>   
     </BrowserRouter>
-    
+  
   );
+  }
 }
 
 export default App;
--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-export default function MainPage(){
+export default function MainPage(props){
+
+  const mainList = props.store.notes.map(note => {
+    return(
+      <li key={note.id}>
+        <p>{note.name}</p>
+        <p>{Date(note.modified)}</p>
+      </li>
+    )
+  })
+
+console.log(props.store)
+
   return(
     <div className ="MainPage">
-    <p>Main Page</p>
+      {mainList}
     </div>
 
   );
